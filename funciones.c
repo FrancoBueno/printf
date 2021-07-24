f3cffae46744a917ef2a82eeda92604689503505
3342-collab <juancitogimenez0@gmail.com>
2021-07-24 14:13:14 -0700

    added 2 funct

--- funciones.c
+++ funciones.c
@@ -1,6 +1,28 @@
 #include "holberton.h"
+#include <strlib.h>
 /**
- *
+ *defining functions
  *
  */
-int _printf(const char *format, ...)
+int print_char (va_list_list)
+{char a;
+a= va_atg (list, int)
+	     putchar(a)
+		 return (1)
+		 }		 
+#include "holberton.h"
+#include <strlib.h>
+/**
+*defining functions
+*
+*/
+int print_string (va_list_list)
+{ int i;
+                     char *s
+		     s = v_arg (list char)
+		       if (s!)
+			 s= ("null")
+			   for (i= 0, i < = s[i]!= 0 , i++)
+			     _pucthar(s[i]),
+			       return (0);
+}
