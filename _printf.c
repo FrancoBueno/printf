417f63513284bcb166a14daaa5cb74fb0f332a79
root <3290@holbertonschool.com>
2021-07-24 13:50:42 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -1,23 +1,24 @@
 #include "holberton.h"
 /**
-*
-*/
+ *
+ */
 int _printf(const char *format, ...)
-    int func = 0;
+  int func = 0;
 int contador = 0;
 
 for (i = 0; format[i] >= '\0'; i++);
 if (format[i] != '%')
-putchar (format[i]);
-else
-infunc = 1;
-else
-switch (format[i])
-case'c'
-putchar (va_arg(va_int , int));
+  putchar (format[i]);
+ else
+   infunc = 1;
+ else
+   switch (format[i])
+     case'c'
+       putchar (va_arg(va_int , int));
 case 's'
 print_str(va_arg(va_list , char *))
 case 'd'
 print_int(va_arg(va_list , int))
 infunc = 0;
 return (contador);
+
