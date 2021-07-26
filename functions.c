abba271360cf30520bb7d2e98620a9816212d489
root <3290@holbertonschool.com>
2021-07-26 14:32:14 -0700

    print_str

--- functions.c
+++ functions.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 
 /**
- *
- *
+ *print_str - print a string of char
+ *@c: pointer to string
+ *Return: string of char
  */
 
 int print_str(char *c)
 {
 	int i;
+
 	if (c)
+
 	{
 		for (i = 0; c[i] != '\0'; i++)
 		{
