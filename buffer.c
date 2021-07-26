0b0d4e9d3b810902e7bb3de9665077ceec97ffe3
3342-collab <juancitogimenez0@gmail.com>
2021-07-26 09:27:56 -0700

    buffer.c

---
+++ buffer.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ *printfbuffer - prints the buffer only if the number of characters = 1024
+ *@buffer: the buffer being printed
+ *@buffer:buffer itf
+ *Return: 1024 
+ */
+int printf_buffer(char *buffer)
+{
+write (1, buffer, 1024);
+return (1024);
+}
