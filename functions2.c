ab4e3befaa480a8ad7020ee2440b5536904e7d02
root <3290@holbertonschool.com>
2021-07-27 17:23:49 -0700

    bin

--- functions2.c
+++ functions2.c
@@ -27,3 +27,27 @@ int print_hexlow(unsigned int num)
     }
   return (count);
 }
+/**
+  * print_bin - print the binary of the number
+* @num: int
+  *
+* Return: number of bytes
+  */
+int print_bin(unsigned int num)
+{
+  int count = 0;
+
+  if (num)
+    {
+      if (num >= 1)
+	{
+	  count += print_bin(num / 2);
+	  count += _putchar((num % 2) + '0');
+	}
+      else
+	{
+	  return (-1);
+	}
+    }
+  return (count);
+}
